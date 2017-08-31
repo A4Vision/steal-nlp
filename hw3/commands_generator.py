30941adfc378bfeb3fdc87bd558b9707be2fe10d
Assaf <assafyifrach@mail.tau.ac.il>
2017-09-01 02:21:30 +0300

    Correct commands generation to use different amounts of words.

--- hw3/commands_generator.py
+++ hw3/commands_generator.py
@@ -11,16 +11,19 @@ from hw3 import labels_only
 
 def parameterize_file_name(args, file_name):
     return "{}_words{}_strategy{}_l2{}_loss{}".format(file_name, args.num_words,
-                                                        args.strategy, args.l2_weight,
-                                                        args.loss_improvement)
+                                                      args.strategy, args.l2_weight,
+                                                      args.loss_improvement)
+
 
 def command(words, strategy, l2_weight, loss_improvement,
             original_model_fname, minimal_frequency):
     FORMAT = "python hw3/labels_only.py --original_model_file_name={0} --stolen_model_file_name=stolen --eta={1} " \
-             "--l2_weight={2} --loss_improvement={3} --minimal_frequency={4} --total_queries_amount={5} --batch_size={6} " \
-             "--strategy={7} --first_random={8} --num_words={8} >& ~/outputs/output_labels_only_freq{4}_words{8}_l2_weight{2}_lossimpr{3}_eta{1}_{7}.txt &"
+             "--l2_weight={2} --loss_improvement={3} --minimal_frequency={4} --total_queries_amount={5} " \
+             "--batch_size={6} " \
+             "--strategy={7} --first_random=1000 --num_words={8} >& ~/outputs/output_labels_only_freq{4}_words{" \
+             "8}_l2_weight{2}_lossimpr{3}_eta{1}_{7}.txt & "
     return FORMAT.format(original_model_fname, 6., l2_weight, loss_improvement, minimal_frequency, 100000,
-                         600, strategy, 600, words)
+                         600, strategy, words)
 
 
 def main():
